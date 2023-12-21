6a0403b7d18da0697697607e26a7f5861a4d1a70
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2023-12-21 12:27:40 +0530

    Time: 9 ms (69.19%) | Memory: 58.5 MB (26.02%) - LeetSync

--- 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     int dp[][];
     public int help(int []nums,int []mul,int s,int i){
         int m=mul.length;
@@ -20,5 +20,21 @@ class Solution {
             }
         }
         return help(nums,multipliers,0,0);
+    }
+}
+class Solution {
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int m=multipliers.length;
+        int n=nums.length;
+        int [][]dp=new int[m+1][m+1];
+        for(int i=m-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int s=multipliers[i]*nums[j]+dp[i+1][j+1];
+                int right=(n-1)-(i-j);
+                int e=multipliers[i]*nums[right]+dp[i+1][j];
+                dp[i][j]=Math.max(s,e);
+            }
+        }
+        return dp[0][0];
     }
 }
