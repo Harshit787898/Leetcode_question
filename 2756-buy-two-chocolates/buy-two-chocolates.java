3513f3004f31742b5acbad362b4a0bfecde31f35
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2023-12-21 11:06:50 +0530

    Time: 2 ms (79.00%) | Memory: 45.1 MB (5.58%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+         Arrays.sort(prices);
+        int sum=prices[0]+prices[1];
+        if(sum>money)
+        return money;
+        else
+        return money-sum;
+    }
+}
