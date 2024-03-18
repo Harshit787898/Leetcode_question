13b6c97b9233ea4d8f0fd391491b12919173e96e
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-03-18 11:40:55 +0530

    Time: 62 ms (17.66%) | Memory: 68.9 MB (54.53%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -3,8 +3,7 @@ class Solution {
         Arrays.sort(points,(a, b) -> Double.compare(a[1], b[1]));
         int ans = 1;
         int target = points[0][1];
-        
-        for (int i=1;i<points.length;i++) {
+        for (int i=0;i<points.length;i++) {
             if (points[i][0]>target) {
                 ans++;
                 target=points[i][1];
