300979fa691018d810f8ebc66a7426e89ba66d9b
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-03-21 12:26:06 +0530

    Time: 2202 ms (5.04%) | Memory: 55.1 MB (89.78%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean dfs(int node,int[][] graph,boolean visited[],boolean path[],List<Integer> ans){
+    public boolean dfs(int node,int[][] graph,boolean visited[],boolean path[],List<Integer> cycle){
         visited[node]=true;
         path[node]=true;
         for(int i:graph[node]){
             if(visited[i]==false){
-                if(dfs(i,graph,visited,path,ans)){
-                    ans.add(i);
+                if(dfs(i,graph,visited,path,cycle)){
+                    cycle.add(i);
                     return true;
                 }
             }
             else if(path[i]==true){
-                ans.add(i);
+                cycle.add(i);
                 return true;
             }
         }
@@ -19,19 +19,19 @@ class Solution {
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n=graph.length;
-        List<Integer> ans=new ArrayList<>();
+        List<Integer> cycle=new ArrayList<>();
         boolean visited[]=new boolean[n];
         boolean path[]=new boolean[n];
         for(int i=0;i<n;i++){
             if(visited[i]==false){
-                if(dfs(i,graph,visited,path,ans)){
-                    ans.add(i);
+                if(dfs(i,graph,visited,path,cycle)){
+                    cycle.add(i);
                 }
             }
         }
         List<Integer> res=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(!ans.contains(i)){
+            if(!cycle.contains(i)){
                 res.add(i);
             }
         }
