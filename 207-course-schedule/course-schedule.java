8a3555cdefde037336e64175fd37a38b56689fbb
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-03-21 11:49:54 +0530

    Time: 6 ms (49.36%) | Memory: 55.3 MB (5.54%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,4 +1,5 @@
-class Solution {
+//Creating a graph and detecting cycle-: if cycle found then return false else return true (DFS Approach)
+class Solution1 {
     public boolean dfs(int node,List<List<Integer>> graph,boolean visited[],boolean path[]){
         visited[node]=true;
         path[node]=true;
@@ -33,5 +34,41 @@ class Solution {
             }
         }
         return true;
+    }
+}
+
+//Creating a graph and detecting cycle-: if cycle found then return false else return true (BFS Approach)
+class Solution{
+    public boolean canFinish(int n, int[][] course) {
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>(i));
+        }
+        int indegree[]=new int[n];
+        for(int c[]:course){
+            graph.get(c[1]).add(c[0]);
+            indegree[c[0]]+=1;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int x=q.poll();
+            ans.add(x);
+            for(int i:graph.get(x)){
+                indegree[i]-=1;
+                if(indegree[i]==0){
+                    q.add(i);
+                }
+            }
+        }
+        if(ans.size()==n){
+            return true;
+        }
+        return false;
     }
 }
