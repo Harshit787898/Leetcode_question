0b4ecc21f6000e93114f05b0e5877e08982317db
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-03-28 14:15:04 +0530

    Time: 29 ms (22.67%) | Memory: 55.1 MB (81.74%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public boolean dfs(int node,int[][] graph,boolean visited[],boolean path[],List<Integer> cycle){
         visited[node]=true;
         path[node]=true;
@@ -36,5 +36,47 @@ class Solution {
             }
         }
         return res;
+    }
+}
+
+//S1-: Reverse all edges
+//S2-: Find Indegrees in the graph
+//S3-: Apply Topological sort
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> safe=new ArrayList<>();
+        List<List<Integer>> revgraph=new ArrayList<>();
+        int indegree[]=new int[graph.length];
+        for(int i=0;i<graph.length;i++){
+            revgraph.add(new ArrayList<>());
+        }
+
+        //Reversing the graph and calculating indegrees
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].length;j++){
+                revgraph.get(graph[i][j]).add(i);
+                indegree[i]+=1;
+            }
+        }
+
+        //BFS Topological Sort
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<revgraph.size();i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int x=q.poll();
+            safe.add(x);
+            for(int v:revgraph.get(x)){
+                indegree[v]-=1;
+                if(indegree[v]==0){
+                    q.offer(v);
+                }
+            }
+        }
+        Collections.sort(safe);
+        return safe;
     }
 }
