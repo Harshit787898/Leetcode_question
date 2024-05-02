4ee5317f8939e3c9535406c5875cc42d83919213
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-05-02 14:02:32 +0530

    Time: 17 ms (36.40%) | Memory: 44.9 MB (36.75%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<String> powerset=new ArrayList<>();
+    public void permute(StringBuilder s, int n) {
+        if (s.length() == n) {
+            powerset.add(s.toString());
+            return;
+        }
+        char ch[] = { 'a', 'b', 'c' };
+        for (int i = 0; i < 3; i++) {
+            if (s.length() == 0 || ch[i] != s.charAt(s.length() - 1)) {
+                s.append(ch[i]);
+                permute(s, n);
+                s.deleteCharAt(s.length() - 1);
+            }
+        }
+    }
+
+    public String getHappyString(int n, int k) {
+        StringBuilder str = new StringBuilder();
+        permute(str, n);
+        Collections.sort(powerset);
+        return (k <= powerset.size()) ? powerset.get(k - 1) : "";
+    }
+}
