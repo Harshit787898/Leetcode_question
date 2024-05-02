9e17bda2f490d1a8b5c63f1f68db7e9c3a868dbe
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-05-02 14:09:22 +0530

    Time: 7 ms (63.25%) | Memory: 44.5 MB (47.70%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,15 +1,17 @@
 class Solution {
     List<String> powerset=new ArrayList<>();
-    public void permute(StringBuilder s, int n) {
+    public void permute(StringBuilder s, int n,int k) {
         if (s.length() == n) {
             powerset.add(s.toString());
             return;
         }
+        if(powerset.size()==k)
+        return ;
         char ch[] = { 'a', 'b', 'c' };
         for (int i = 0; i < 3; i++) {
             if (s.length() == 0 || ch[i] != s.charAt(s.length() - 1)) {
                 s.append(ch[i]);
-                permute(s, n);
+                permute(s,n,k);
                 s.deleteCharAt(s.length() - 1);
             }
         }
@@ -17,8 +19,7 @@ class Solution {
 
     public String getHappyString(int n, int k) {
         StringBuilder str = new StringBuilder();
-        permute(str, n);
-        // Collections.sort(powerset);
+        permute(str, n,k);
         return (k <= powerset.size()) ? powerset.get(k - 1) : "";
     }
 }
