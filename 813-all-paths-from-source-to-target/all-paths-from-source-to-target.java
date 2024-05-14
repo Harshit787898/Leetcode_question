c5b12d2fe90646660dcd69bb47dc86c785d09618
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-05-14 12:18:36 +0530

    Time: 2 ms (77.27%) | Memory: 46.2 MB (59.90%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void solve(int[][]graph,List<Integer> path,List<List<Integer>>paths,int i,int j){
+        if(i==j){
+            paths.add(new ArrayList(path));
+        }
+        else{
+            for(int neigh:graph[i]){
+                path.add(neigh);
+                solve(graph,path,paths,neigh,j);
+                path.remove(path.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> paths=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        path.add(0);
+        solve(graph,path,paths,0,graph.length-1);
+        return paths;
+    }
+}
