4d7aed21de4a26ef47376ec976f0a7e6b4ad5702
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-06-14 01:21:02 +0530

    Time: 4 ms (5.41%) | Memory: 43.7 MB (90.31%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -1,10 +1,17 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
+        PriorityQueue<Integer> pq1=new PriorityQueue<>();
+        PriorityQueue<Integer> pq2=new PriorityQueue<>();
+        int n=students.length;
         int ans=0;
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        for(int i=0;i<students.length;i++){
-            ans+=Math.abs(students[i]-seats[i]);
+        for(int i=0;i<n;i++){
+            pq1.offer(seats[i]);
+            pq2.offer(students[i]);
+        }
+        while(!pq1.isEmpty()){
+            int t1=pq1.poll();
+            int t2=pq2.poll();
+            ans+=Math.abs(t1-t2);
         }
         return ans;
     }
