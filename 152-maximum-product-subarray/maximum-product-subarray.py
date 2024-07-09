44012dfc7c8985fc0938f2cd8b2ec48057d69700
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-07-09 14:12:01 +0530

    Time: 82 ms (10.98%) | Memory: 17.1 MB (28.78%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans = float('-inf')
+        p = 1
+        for num in nums:
+            if num == 0:
+                ans = max(ans, 0)
+                p = 1
+                continue
+            else:
+                p *= num
+                ans = max(num, max(p, ans))
+        p = 1
+        for num in reversed(nums):
+            if num == 0:
+                ans = max(ans, 0)
+                p = 1
+                continue
+            else:
+                p *= num
+                ans = max(num, max(p, ans))
+        
+        return ans
