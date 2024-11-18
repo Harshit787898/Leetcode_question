a0502d3c4c73eada3fd7227f0222e54ce322704b
Harshit Singh <119102073+Harshit787898@users.noreply.github.com>
2024-11-18 14:59:36 +0530

    Time: 1 ms (71.00%) | Memory: 42.3 MB (36.94%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int sumg(int code[],int i,int k){
+        int s=0;i=i+1;
+        while(k>0){
+            if(i>code.length-1){
+                 i=0;
+                  s+=code[i];
+            }
+            else{
+                s+=code[i];
+            }
+            i+=1;
+            k--;
+        }
+        return s;
+    }
+    public int suml(int code[],int i,int k){
+       int s=0;i=i-1;
+       while(k>0){
+           if(i>=0&&i<code.length){
+               s+=code[i];
+           }
+           else{
+               i=code.length-1;
+               s+=code[i];
+           }
+           i-=1;
+           k-=1;
+       }
+       return s;
+    }
+    public int[] decrypt(int[] code, int k) {
+        int ans[]=new int[code.length];
+        if(k==0){
+            for(int i=0;i<code.length;i++){
+                ans[i]=0;
+            }
+            return ans;
+        }
+        else if(k>0){
+            for(int i=0;i<code.length;i++){
+                ans[i]=sumg(code,i,k);
+            }
+            return ans;
+        }
+        else{
+            for(int i=0;i<code.length;i++){
+                ans[i]=suml(code,i,k*(-1));
+            }
+            return ans;
+        }
+    }
+}
